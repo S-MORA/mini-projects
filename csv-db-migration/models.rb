28458798f9baa0d1cc6f0365bbdee3e47b0f1146
Sharon Morato <sharonmorato1@gmail.com>
2018-05-29 12:07:12 -0400

    last

--- csv-db-migration/models.rb
+++ csv-db-migration/models.rb
@@ -14,6 +14,6 @@ class PurchaseTransaction < ActiveRecord::Base
 
   def sale_price
     #this outputs the final sale price plus 8.8 percent sales tax
-    car.cost_price + (car.cost_price + car.sale_markup/100) + (car.cost_price * .088)
+    car.cost_price + (car.cost_price * car.sale_markup/100) + (car.cost_price * .088)
   end
 end
