746bcab2b7cb3a2ccef0f175420c23ca64bb9767
Sharon Morato <sharonmorato1@gmail.com>
2018-05-29 11:52:57 -0400

    last

--- csv-db-migration/models.rb
+++ csv-db-migration/models.rb
@@ -1,9 +1,5 @@
 require 'sinatra/activerecord'
 
-# Based on http://www.jonathanleighton.com/articles/2011/awesome-active-record-bug-reports/
-
-
-# Define the models and relationships
 class Car < ActiveRecord::Base
   has_one :purchase_transaction
 end
@@ -15,4 +11,9 @@ end
 class PurchaseTransaction < ActiveRecord::Base
   belongs_to :car
   belongs_to :customer
+
+  def sale_price
+    #this outputs the final sale price plus 8.8 percent sales tax
+    car.cost_price + (car.cost_price + car.sale_markup/100) + (car.cost_price * .088)
+  end
 end
--- csv-db-migration/populate_db_script.rb
+++ csv-db-migration/populate_db_script.rb
@@ -2,35 +2,23 @@ require './models'
 require 'csv'
 
 CSV.foreach('./CAR_DATA.csv') do |row|
-  unless row[0] == 'id'
+ unless row[0] == 'id'
    my_customer =  Customer.create(
       first_name: row[1],
       last_name: row[2],
       email: row[3],
       gender: row[4],
       phone_number: row[5]
-     )
+    )
    my_car = Car.create(
     model: row[6],
     make: row[7],
     cost_price: row[9],
     sale_markup: row[10]
     )
-   Transaction.create (
+   PurchaseTransaction.create (
      car: my_car,
      customer: my_customer
     )
+  end
 end
-
-
-# Everytime the script is run it clears the database, this
-# is okay while working on carson's request.
-# Car.destroy_all
-# Customer.destroy_all
-# Transaction.destroy_all
-
-# Create a few records...
-# or import that CSV and create the appropriate records off of it.
-
-
-# to let you use the terminal to CRUD the database.
