33756c3804c4bdfc908c268b61cef1036a6df11b
Sharon Morato <sharonmorato1@gmail.com>
2018-06-04 17:08:53 -0400

    delete & edit

--- rails_todos/app/controllers/todos_controller.rb
+++ rails_todos/app/controllers/todos_controller.rb
@@ -3,15 +3,31 @@ class TodosController < ApplicationController
     @todos = Todo.all
   end
   def new
+    @todo = Todo.new
   end
   def show
   end
   def create
+    Todo.create(todo_params) # creates within DB
+    redirect_to todos_path
   end
   def edit
+    @todo = Todo.find(params[:id])
   end
   def update
+    todo = Todo.find(params[:id])
+    todo.update(todo_params)
+    redirect_to todos_path
   end
   def destroy
+    Todo.destroy(params[:id])
+    redirect_to todos_path
   end
+
+  private ##strong parameters
+  # returns {name: 'userinput', description: 'userinput'}
+  def todo_params
+    params.require(:todo).permit(:name, :description)
+  end
+
 end
