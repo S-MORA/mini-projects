d4b65ad24a60c35700c267e5af683099f8541e99
Sharon Morato <sharonmorato1@gmail.com>
2018-06-05 14:33:33 -0400

    first

---
+++ rails_bakery/app/controllers/muffins_controller.rb
@@ -0,0 +1,32 @@
+class MuffinsController < ApplicationController
+  def index
+   @muffins = Muffin.all
+  end
+  def new
+   @muffin = Muffin.new
+  end
+  def edit
+   @muffin = Muffin.find(params[:id])
+  end
+  def show
+   @muffin = Muffin.find(params[:id])
+  end
+  def create
+   Muffin.create(muffin_params)
+   redirect_to muffins_path
+  end
+  def update
+   muffin = Muffin.find(params[:id])
+   muffin.update(muffin_params)
+   redirect_to muffins_path
+  end
+ def destroy
+  Muffin.destroy(params[:id])
+  redirect_to muffins_path
+ end
+private
+ def muffin_params
+  params.require(:muffin).permit(:name, :description, :category, :price, :available_until)
+ end
+
+end
