80eca2f29c8c30b71f749adee4c813cc3e241453
Sharon Morato <sharonmorato1@gmail.com>
2018-06-07 20:47:48 -0400

    experimentation

--- rails_todos/config/routes.rb
+++ rails_todos/config/routes.rb
@@ -1,6 +1,23 @@
 Rails.application.routes.draw do
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
-  
-  resources :todos
 
+  resources :todos do
+    resources :todo_comments, only: [:new, :create]
+
+    # collection do
+    #   get'/search', to: 'todos#search'
+    #   get'/visible', to: 'todos#visible'
+    #   get'/hidden', to: 'todos#hidden'
+    #   post '/mark_as_read', to: 'todos#mark_as_read'
+    # end
+    #
+    # member do
+    #   get 'mark_as_complete', to: 'todos#mark_as_complete'
+    #   get '/hide', to: 'todos#hide'
+    #   get '/share', to: 'todos#share'
+    # end
+
+  end
+
+ # resources :todo_comments
 end
