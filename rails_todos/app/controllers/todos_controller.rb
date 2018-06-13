fd37c9e6955491d8fa44977915c7690fda7fe283
Sharon Morato <sharonmorato1@gmail.com>
2018-06-13 12:41:11 -0400

    activerecord validation

--- rails_todos/app/controllers/todos_controller.rb
+++ rails_todos/app/controllers/todos_controller.rb
@@ -1,4 +1,5 @@
 class TodosController < ApplicationController
+  before_action :authenticate_user!
   def index
     @todos = Todo.all
   end
@@ -9,9 +10,14 @@ class TodosController < ApplicationController
     @todo = Todo.find(params[:id])
   end
   def create
-    Todo.create(todo_params) # creates within DB
-    flash[:success] = "Your todo has been created"
-    redirect_to todos_path
+    @todo = Todo.create(todo_params)
+    if @todo.valid?
+      flash[:success] = "Your todo has been created"
+      redirect_to todos_path
+    else
+      flash[:error] = 'Invalid'
+      render :new
+    end
   end
   def edit
     @todo = Todo.find(params[:id])
@@ -29,7 +35,7 @@ class TodosController < ApplicationController
   private ##strong parameters
   # returns {name: 'userinput', description: 'userinput'}
   def todo_params
-    params.require(:todo).permit(:name, :description)
+    params.require(:todo).permit(:name, :description, :priority)
   end
 
 end
--- rails_todos/app/models/todo.rb
+++ rails_todos/app/models/todo.rb
@@ -1,3 +1,6 @@
 class Todo < ApplicationRecord
+  validates :name, presence: true
+  validates :name, length: { minimum: 4 }
+  validates :priority, inclusion: { in: ['H','M','L'], message: "Please select a valid priority"}
   has_many :todo_comments, dependent: :destroy
 end
